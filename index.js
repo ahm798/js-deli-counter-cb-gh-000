481a0fb746b39349d05698572a857c2cecfba663
ahmed abd elwahab <ahmed.almelegy617@el-eng.menofia.edu.eg>
2019-11-29 17:47:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ function takeANumber(arr, name){
 
 function currentLine(arr){
     let s = ``;
-    for(let i =0; i < arr.length; i++){
-        s += `${i + 1}. ${arr[i]}, `;
+    if(arr.length) {
+        for (let i = 0; i < arr.length; i++) {
+            s += `${i + 1}. ${arr[i]}, `;
+        }
+    }
+    else{
+        return "The line is currently empty.";
     }
     return "The line is currently: " + s.slice(0, -2);
 }
 
 function nowServing(arr){
-    return `Currently serving ${arr[arr.length - 1]}`;
+    return `Currently serving ${arr.shift()}`;
 }
